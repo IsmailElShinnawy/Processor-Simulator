7443bbfca2fa74547b4980524c1aa3ec85e67984
mzakariia <mohamed.zakariia1@gmail.com>
2021-06-14 20:59:21 +0200

    Completed Memory implementation with exceptions

---
+++ src/exceptions/MemoryReadException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class MemoryReadException extends Exception{
+    
+    public MemoryReadException(String s){
+        super(s);
+    }
+}
---
+++ src/exceptions/MemoryWriteException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class MemoryWriteException extends Exception {
+    public MemoryWriteException(String s) {
+        super(s);
+    }
+}
